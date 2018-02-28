ec431e895f937d155393bd1f4690eee9495fec1c
liuxiaohuaharoro <1379744042@qq.com>
2018-02-28 14:38:15 +0800

    talks-list

--- app/static/js/public.js
+++ app/static/js/public.js
@@ -1,1 +1,12 @@
-document.write("<script src='{{ url_for('static',filename='js/jquery.min.js')}}'></script>")
+$(document).ready(function(){
+   
+    $('#goTop').click(function(){
+        $('html, body').animate({scrollTop: '0px'}, 800);
+    });
+    $('#refresh').click(function(){
+        window.location.reload();
+    });
+    $('#goBottom').click(function(){
+        $('html, body').animate({scrollTop: $('.footer').offset().top}, 800);
+    });
+});
